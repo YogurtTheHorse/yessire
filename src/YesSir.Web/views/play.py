720f008b271efec44def99f4ddb3593309a3417d
Seryiza <seryiza@yandex.ru>
2017-02-18 15:41:21 +1100

    Add send-method, not work c.c

--- src/YesSir.Web/views/play.py
+++ src/YesSir.Web/views/play.py
@@ -1,5 +1,6 @@
 from app import app, socketio
 from flask import render_template
+from flask_socketio import send
 from urllib.request import urlopen
 
 @app.route('/play')
@@ -12,17 +13,16 @@ def connect():
 
 @socketio.on('startGame')
 def handle_start(id):
-    return method('/start/web/%s' % id)
+    return send(method('/start/web/%s' % id))
 
 @socketio.on('message')
-def handle_message(data):
-    return method('/message/web/%s/%s' % (data.id, data.msg))
+def handle_message(json):
+    return send(method('/message/web/%s/%s' % (json.id, json.msg)))
 
 @socketio.on('getMessages')
 def handle_get(id):
-    return method('/get/web/%s' % id)
+    return send(method('/get/web/%s' % id))
 
 def method(path):
     '''HTTP-request'''
-    print(path)
     return urlopen('http://localhost:9797' + path).read()
