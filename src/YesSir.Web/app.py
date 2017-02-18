bc5620c8c4d86a85072f7b068106f30b44e8c68c
Seryiza <seryiza@yandex.ru>
2017-02-18 12:44:25 +1100

    Add flask-socketio

--- src/YesSir.Web/app.py
+++ src/YesSir.Web/app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
 from views import main_page
+from views import play
--- src/YesSir.Web/run.py
+++ src/YesSir.Web/run.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, socketio
 
 print('Starting Flask server...')
-app.run()
+socketio.run(app)
