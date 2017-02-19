bd2d37816b0bd427bee5c7a2b3803c30d8bac343
yegorf1 <yegorf1@gmail.com>
2017-02-19 15:02:51 +0300

    Added AvaibalePlace in Building

--- src/YesSir.Backend/Entities/Kingdoms/Building.cs
+++ src/YesSir.Backend/Entities/Kingdoms/Building.cs
@@ -5,12 +5,28 @@ namespace YesSir.Backend.Entities.Kingdoms {
 	[MoonSharp.Interpreter.MoonSharpUserData]
 	public class Building : IUsable {
 		public Guid Id { get; set; }
+		public int AvaibalePlace = 1; // Count of human that can be at once in building
+		public int UsedPlace = 0;
 		public Guid KingdomId;
 
 		public string Name;
 		public float Quality;
 
-		public bool IsBusy { get; set; } = false;
+		public bool IsBusy
+		{
+			get
+			{
+				return AvaibalePlace <= UsedPlace;
+			}
+			set
+			{
+				if (value) {
+					UsedPlace++;
+				} else {
+					UsedPlace--;
+				}
+			}
+		}
 
 		public string GetName(string language) {
 			return Locale.Get("buildings." + Name + ".name", language);
